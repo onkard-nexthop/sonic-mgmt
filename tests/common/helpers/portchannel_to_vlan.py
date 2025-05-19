a1c3adddc85da1153fd11dab33df2ce201412370
Onkar Deshpande <onkard@nexthop.ai>
2025-05-19 16:19:05 -0700

    Fix possible UnboundLocalError in setup_po2vlan
    If `setup_dut_ptf()` raises an exception, then the variable `ptf_lag_map` will not be set and a `UnboundLocalError` will be raised in the `finally` block. We fix this by checking if the variable has been initialized before cleaning it.

--- tests/common/helpers/portchannel_to_vlan.py
+++ tests/common/helpers/portchannel_to_vlan.py
@@ -421,6 +421,7 @@ def setup_po2vlan(duthosts, ptfhost, rand_one_dut_hostname, rand_selected_dut, p
         yield
         return
     # --------------------- Setup -----------------------
+    ptf_lag_map = None
     try:
         dut_lag_map, ptf_lag_map, src_vlan_id = setup_dut_ptf(ptfhost, duthost, tbinfo, vlan_intfs_dict)
 
@@ -431,7 +432,8 @@ def setup_po2vlan(duthosts, ptfhost, rand_one_dut_hostname, rand_selected_dut, p
     # --------------------- Teardown -----------------------
     finally:
         config_reload(duthost, safe_reload=True)
-        ptf_teardown(ptfhost, ptf_lag_map)
+        if ptf_lag_map is not None:
+            ptf_teardown(ptfhost, ptf_lag_map)
 
 
 def has_portchannels(duthosts, rand_one_dut_hostname):
